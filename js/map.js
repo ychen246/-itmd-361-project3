be5a79396ea6e324eb2adaeaee0d01cbfd556f31
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2018-11-20 19:17:04 -0600

    Implemented for other marker
    Pop up info for other marker when clicked.

--- js/map.js
+++ js/map.js
@@ -11,7 +11,12 @@ function initMap(){
   var college = {lat:41.834873, lng:-87.627006};
   var marker3 = new google.maps.Marker({position:college, map:map, title:"Illinois Institute of Technology"});
 
-  var infowindow1 = new google.maps.InfoWindow ({content: "This is the Elementary I attended when I moved to America at the age of 6"});
+  var infowindow1 = new google.maps.InfoWindow ({content: "James Ward Elementary is the school I attended when I moved to America at the age of 6"});
   marker1.addListener('click', function() {infowindow1.open(map, marker1); });
   
+  var infowindow2 = new google.maps.InfoWindow ({content: "Northside College Prep is the high school I attended. It is the number one public high school in Chicago."});
+  marker2.addListener('click', function() {infowindow2.open(map, marker2); });
+
+  var infowindow3 = new google.maps.InfoWindow ({content: "Illinois Institute of Technology, IIT, is the university I am currently attending."});
+  marker3.addListener('click', function() {infowindow3.open(map, marker3); });
 }
