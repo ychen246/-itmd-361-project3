95aa00b4cb8d3e2cb465915077c4e19f20103f99
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2018-11-20 19:13:54 -0600

    testing event listener
    adds info to the marker when clicked

--- js/map.js
+++ js/map.js
@@ -3,11 +3,15 @@ function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {zoom:10.5, center:chicago});
 
   var elementaryschool = {lat:41.843505, lng:-87.635281};
-  var marker = new google.maps.Marker({position:elementaryschool, map:map, title:"James Ward Elementary"});
+  var marker1 = new google.maps.Marker({position:elementaryschool, map:map, title:"James Ward Elementary"});
 
   var highschool = {lat:41.981306, lng:-87.7079};
-  var marker = new google.maps.Marker({position:highschool, map:map, title:"Northside College Prep"});
+  var marker2 = new google.maps.Marker({position:highschool, map:map, title:"Northside College Prep"});
 
   var college = {lat:41.834873, lng:-87.627006};
-  var marker = new google.maps.Marker({position:college, map:map, title:"Illinois Institute of Technology"});
+  var marker3 = new google.maps.Marker({position:college, map:map, title:"Illinois Institute of Technology"});
+
+  var infowindow1 = new google.maps.InfoWindow ({content: "This is the Elementary I attended when I moved to America at the age of 6"});
+  marker1.addListener('click', function() {infowindow1.open(map, marker1); });
+  
 }
