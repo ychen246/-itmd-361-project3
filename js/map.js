94eaa7edc2b634c0e46b4068e0219e01a13b8678
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2018-11-20 19:04:53 -0600

    Add Titles to Marker

--- js/map.js
+++ js/map.js
@@ -3,11 +3,11 @@ function initMap(){
   var map = new google.maps.Map(document.getElementById('map'), {zoom:10.5, center:chicago});
 
   var elementaryschool = {lat:41.843505, lng:-87.635281};
-  var marker = new google.maps.Marker({position:elementaryschool, map:map});
+  var marker = new google.maps.Marker({position:elementaryschool, map:map, title:"James Ward Elementary"});
 
   var highschool = {lat:41.981306, lng:-87.7079};
-  var marker = new google.maps.Marker({position:highschool, map:map});
+  var marker = new google.maps.Marker({position:highschool, map:map, title:"Northside College Prep"});
 
   var college = {lat:41.834873, lng:-87.627006};
-  var marker = new google.maps.Marker({position:college, map:map});
+  var marker = new google.maps.Marker({position:college, map:map, title:"Illinois Institute of Technology"});
 }
