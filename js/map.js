dc358e8c92e1c13920a47ac81e6153995b8024d5
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2018-11-20 18:24:36 -0600

    Map Initialized

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
-function initMapmap(){
-
+function initMap(){
+  var chicago = {lat:41.878114 , lng:-87.629798};
+  var map = new google.maps.Map(document.getElementById('map'), {zoom:4, center:chicago});
 }
