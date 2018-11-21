59f37bb899bfe35a9f62dbc9a082d750fc484da5
Yujun Chen <33580541+ychen246@users.noreply.github.com>
2018-11-20 19:01:40 -0600

    Added map markers

--- js/map.js
+++ js/map.js
@@ -1,4 +1,13 @@
 function initMap(){
   var chicago = {lat:41.878114 , lng:-87.629798};
-  var map = new google.maps.Map(document.getElementById('map'), {zoom:4, center:chicago});
+  var map = new google.maps.Map(document.getElementById('map'), {zoom:10.5, center:chicago});
+
+  var elementaryschool = {lat:41.843505, lng:-87.635281};
+  var marker = new google.maps.Marker({position:elementaryschool, map:map});
+
+  var highschool = {lat:41.981306, lng:-87.7079};
+  var marker = new google.maps.Marker({position:highschool, map:map});
+
+  var college = {lat:41.834873, lng:-87.627006};
+  var marker = new google.maps.Marker({position:college, map:map});
 }
